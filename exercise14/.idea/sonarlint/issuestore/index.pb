ba8d8943159a664033bd7f4164f39758629e4cc3
Alexander <azoomer2@gmail.com>
2021-09-19 17:51:02 -0400

    Cleaned up files for final commit

---
+++ exercise14/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+M
+src/main/java/Solution14.java,8\7\876423c435b77a4d1a1a0eb0e2c785358863c146
--- exercise23/src/main/java/Solution23.java
+++ exercise23/src/main/java/Solution23.java
@@ -1,3 +1,8 @@
+/*
+ *   UCF COP3330 Fall 2021 Assignment 2 Solution
+ *   Copyright 2021 Alexander Zommer
+ */
+
 import java.util.Scanner;
 
 public class Solution23 {
